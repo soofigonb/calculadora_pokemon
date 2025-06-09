34761a84e5f5f8f22ef993ab2c304b43ded282d2
Sofía🌷 <sofiapazgonb@gmail.com>
2025-06-09 19:55:45 -0400

    incorporacion_resta
    Se agregó función resta

--- calculadora.py
+++ calculadora.py
@@ -2,3 +2,8 @@ v1 = int(input("Ingrese valor: "))
 v2 = int(input("Ingrese valor: "))
 
 print("La suma es: ",v1+v2)
+
+v1 = int(input("Ingrese valor: "))
+v2 = int(input("Ingrese valor: "))
+
+print("La resta es: ",v1-v2)
